aa961b70856e235e19c756a2c116a42dc6c53d6b
Milen Radkov <milenradkov@me.com>
2020-05-23 19:47:32 +0300

    updated claim procedures, updated sdk

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
-import Ae, { Universal } from '@aeternity/aepp-sdk/es/ae/universal'
-import axios from 'axios'
+const { Universal: Ae, MemoryAccount, Node, Crypto, Aens } = require('@aeternity/aepp-sdk')
+const config = require('./config')
+const axios = require('axios')
 
-const config = require('./config/config')
-const util = require('util');
+// Initialize aeternity SDK client
+let sdk
 
-const keypair = {
-    publicKey: 'ak_2RUVa9bvHUD8wYSrvixRjy9LonA9L29wRvwDfQ4y37ysMKjgdQ',
-    secretKey: '5277f449a5319b744710fc48af8edb5ed6e73218d35647eb7d4d6162ddc88225bb446fc1b78a37c69ffc1e2a61b2a9a10182d3673af2eb1490b3580c46897480'
+// This is used for handling multiple name claims at once
+// as the on-chain actions (transactions) require valid account nonce
+// this will allow transactions to be executed one by one
+// preventing duplicated nonce resulting in tx drop
+let index = 1
+
+const initSDK = async (keypair = config.keypair) => {
+  sdk = await Ae({
+    nodes: [
+      {
+        name: 'someNode',
+        instance: await Node({
+          url: config.networks[config.selectedNetwork].nodeUrl,
+          internalUrl: config.networks[config.selectedNetwork].nodeUrl,
+        }),
+      },
+    ],
+    compilerUrl: config.networks[config.selectedNetwork].compilerUrl,
+    accounts: [MemoryAccount({ keypair: keypair })],
+    address: keypair.publicKey,
+  })
+
+  const height = await sdk.height()
+  console.log(`SDK initialized by ${keypair.publicKey} ! Current network height: ${height}`)
+
+  config.names.forEach(async (name, index) => await claimOrBid(name, index).catch((e) => e))
 }
 
-const middleware_url = 'https://mainnet.aeternal.io/middleware/';
-let client;
-let bid_step = 1000000;
-let bid_ttl = 5000;
+initSDK()
 
-const initialize_client = async () => {
-    await Universal({
-        url: 'https://sdk-mainnet.aepps.com',
-        internalUrl: 'https://sdk-mainnet.aepps.com',
-        compilerUrl: 'https://latest.compiler.aepps.com',
-        keypair: keypair
-    }).catch(console.error)
-    .then((initialized_client) => {
-        client = initialized_client;
-        auto_bid();
-    })
+const claimOrBid = async (name, index) => {
+  const checkName = await sdk.aensQuery(name).catch((e) => e)
 
+  // check if name is already claimed
+  if (checkName && Object.prototype.hasOwnProperty.call(checkName, 'id')) {
+    console.log('[Name]: ' + name + ' Status: taken')
+    console.log(checkName)
+
+    // check for active auction and bid if there is one
+    console.log('[Name]: ' + name + ' Check for active auction')
+    const bidResult = await autoBid(name)
+    console.log(bidResult)
+  } else {
+    // if name is not registered by anyone then we proceed with claiming
+    console.log('[Name]: ' + name + ' Status: available')
+    setTimeout(async () => {
+      console.log(name, ' : Pre-claim request ...')
+      const preclaimResult = await sdk.aensPreclaim(name)
+      console.log(preclaimResult)
+      console.log(name, ' : Claim request ...')
+      const claimName = await sdk.aensClaim(name, preclaimResult.salt)
+      console.log(claimName)
+    }, config.bid.ttl * (index + 1))
+  }
 }
 
-const auto_bid = async () => {
+const autoBid = async (name) => {
+  console.log(`${config.networks[config.selectedNetwork].middlewareUrl}names/auctions/bids/${name}`)
+  let result = await axios.get(
+    `${config.networks[config.selectedNetwork].middlewareUrl}names/auctions/bids/${name}`,
+  )
+  let activeAuctions = result.data
 
-    let result = await axios.get(`${middleware_url}names/auctions/active`);
-    let active_auctions = result.data;
+  console.log(activeAuctions)
 
-    //loop all the active auctions
-    active_auctions.forEach((auction, index) => {
-        // check if last bidder is not me
-        if (auction.winning_bidder != keypair.publicKey)
-        {
-            setTimeout(function(){
-                //do stuff
-                client.aensBid(
-                    auction.name,
-                    auction.winning_bid + bid_step,
-                    {})
-                .then(result => {
-                    console.log(result);
-                })
-                
-            }, bid_ttl * index)
-        }
-    });
+  //loop all the active auctions
+  activeAuctions.forEach((auction, index) => {
+    // check if last bidder is not me
+    if (auction.winning_bidder != config.keypair.publicKey) {
+      setTimeout(async () => {
+        //do stuff
+        const bid = await sdk.aensBid(auction.name, auction.winning_bid + config.bid.step, {})
+        console.log(bid)
+      }, config.bid.ttl * (index + 1))
+    }
+  })
 }
-
-initialize_client();
